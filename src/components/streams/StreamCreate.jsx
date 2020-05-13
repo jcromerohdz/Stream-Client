5a0268febfb82669b835a2a3eb6b1059da8dd4f0
J. Christian Romero <jcromerohdz@gmail.com>
2020-05-13 15:58:48 -0700

    Adding fetchStream, fetchStreams, editStream, deleteStream actions and fetchingStream functionality

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component{
   renderError({ error, touched }){
@@ -27,8 +29,8 @@ class StreamCreate extends React.Component{
     );
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -44,7 +46,7 @@ class StreamCreate extends React.Component{
 
 const validate = (formValues) => {
   const errors = {}
-  if (!formValues.tilte) {
+  if (!formValues.title) {
     errors.title = 'You must enter a title'
   }
 
@@ -55,7 +57,9 @@ const validate = (formValues) => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped)
