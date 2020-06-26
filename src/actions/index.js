a1d86014e188f6cceb3f32bc2dc9c9456256258e
J. Christian Romero <jcromerohdz@gmail.com>
2020-06-25 18:59:01 -0700

    Adding programatic navigation via createBrowserHistory

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,6 +28,10 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post('/streams', {...formValues, userId});
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  // Do some programatic navigation
+  // get the user back to the root route
+  history.push('/')
+
 };
 
 export const fetchStreams = () => async dispatch => {
